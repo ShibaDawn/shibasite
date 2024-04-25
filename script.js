bb8d6ec4cfd4db1bf18c614e1624aadb71800719
ShibaDawn <167427720+ShibaDawn@users.noreply.github.com>
2024-04-25 12:07:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,17 +8,3 @@ ourImage.addEventListener("click", function() {
     counter = 1;
   }
 });
-
-var myImage = new Image("img1");
-myImage.src = "Images/gal1.jpg";
-
-var spades = [];
-for(var i = 1; i < 14; i++){
-    var spade = new Image();
-    spade.src = "Images/gal"+i+".jpg";
-}
-
-
-
-const image = document.createElement("img");
-image.src = "Images/gal1.jpg";
